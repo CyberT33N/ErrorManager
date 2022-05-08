7e5d9fa22402003c40b652d7f62590fc2f39e9fe
CyberT33N <cybert33n@gmail.com>
2022-05-08 21:37:46 +0200

    refactor(CCS-001): changed complete layout of ErrorManager

--- ErrorManager/ErrorManager.js
+++ ErrorManager/ErrorManager.js
@@ -1,4 +1,4 @@
-/**
+/*
 ███████████████████████████████████████████████████████████████████████████████
 ██******************** PRESENTED BY t33n Software ***************************██
 ██                                                                           ██
@@ -11,7 +11,49 @@
 ██                                                                           ██
 ███████████████████████████████████████████████████████████████████████████████
 ███████████████████████████████████████████████████████████████████████████████
-**/
+*/
+
+/**
+ *
+ */
+const sendError = function() {
+    if(!this.res) {
+        throw new Error('ErrorManager - Can not find this.res')
+    }
+
+    const { e, msg, data, httpStatus } = this
+
+    // If e.message use e.message
+    // If e.message not exist check if e exist
+    // If e not exist check if msg exist
+    // If msg not exist use fallback
+    const errorMessage = e?.message ? e?.message : (e ? e : (msg ? msg : undefined))
+
+    if(!errorMessage){
+        throw new Error('errorMessage is missing')
+    }
+
+    const base = { environment: process.env.npm_lifecycle_event, title: msg }
+
+    // Full error with error message and stack
+    const fullError = {
+        ...base,
+        errorMessage,
+        stack: e?.stack,
+        ...(data ? { data } : {})
+    }
+
+    // If npm_lifecycle_event is start we sanitize the error message and stacktrace
+    const fullErrorSanitized = {
+        ...base,
+        errorMessage: process.env.npm_lifecycle_event === 'start' ? null : errorMessage,
+        stack: process.env.npm_lifecycle_event === 'start' ? null : e?.stack,
+        ...(data ? { data: process.env.npm_lifecycle_event === 'start' ? null : data } : {})
+    }
+
+    this.res.status(httpStatus).json(fullErrorSanitized)
+    throw new Error(JSON.stringify(fullError, null, 4))
+}
 
 /**
  * Handle errors
@@ -34,10 +76,7 @@ class ErrorManager {
             ErrorManager.instance = this
         }
 
-        //- Default values
-        this.httpStatus = 500
-        this.offline = true
-        this.sended = false
+        this.sendError = sendError.bind(this)
 
         return ErrorManager.instance
     }
@@ -56,74 +95,67 @@ class ErrorManager {
 
 /**
  * Default error handling by adding stacktrace
+ * Use any thing else than npm_lifecycle_event=start for including stacktrace and error in response to client
  */
 class BaseError extends ErrorManager {
     /**
      * constructor
+     * @param {object} msg - Error message
      * @param {string} e - Error
-     * @param {string} msg - Error message
      * @param {string} httpStatus - HTTP status code
      * @returns {Promise<*[]>} - void
      */
-    constructor(msg, e, offline = true) {
+    constructor(msg, e) {
         super()
 
-        let errorMessage
+        this.msg = msg
+        this.e = e
+        this.httpStatus = 500
 
-        if(e) {
-            errorMessage = e.message ? e.message : (e ? e : msg)
-        } else {
-            errorMessage = 'No error provided'
-        }
-
-
-        if(!errorMessage && !msg){
-            throw new Error('Error or message missing')
-        }
-
-        this.offline = offline
-
-        this.fullError = {
-            title: msg,
-            errorMessage: process.env.npm_lifecycle_event === 'start' ? null : errorMessage,
-            stack: process.env.npm_lifecycle_event === 'start' ? null : e.stack
-        }
-
-        if(this.res && !this.offline) {
-            this.res.status(this.httpStatus).json(this.fullError)
-            this.sended = true
-        } else {
-            throw new Error(JSON.stringify(this.fullError, null, 4))
-        }
+        this.sendError()
     }
 }
 
 /**
- * Extends BaseError with HTTP Statuscode
+ * Validation Error - Default HTTP Status 400 - Additional with data object
  */
-class RuntimeError extends BaseError {
+class ValidationError extends ErrorManager {
     /**
      * constructor
+     * @param {object} msg - Error message
      * @param {string} e - Error
-     * @param {string} msg - Error message
      * @param {string} httpStatus - HTTP status code
      * @returns {Promise<*[]>} - void
      */
-    constructor(msg, e, httpStatus) {
-        super(msg, e, false)
+    constructor(msg, e, data) {
+        super()
 
-        httpStatus ? httpStatus : this.httpStatus
+        this.msg = msg
+        this.e = e
+        this.data = data
+        this.httpStatus = 400
 
-        if(!this.sended) {
-            this.res.status(httpStatus).send(this.fullError)
-        }
+        this.sendError()
     }
 }
 
-
 // eslint-disable-next-line no-use-before-define
 global.BaseError = BaseError
+
 // eslint-disable-next-line no-use-before-define
-global.RuntimeError = RuntimeError
+global.ValidationError = ValidationError
 
 export default ErrorManager
+
+/*
+
+        // this.constructor.prototype.__proto__ = Error.prototype // Make this an instanceof Error.
+        // Error.call(this) // Does not seem necessary. Perhaps remove this line?
+        // Error.captureStackTrace(this, this.constructor) // Creates the this.stack getter
+        // this.name = this.constructor.name; // Used to cause messages like "UserError: message" instead of the default "Error: message"
+        // this.message = msg; // Used to set the message
+        // this.timestamp = new Date().toISOString()
+
+ */
+
+
